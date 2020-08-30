49df254a2fde4c3bc20ab8e91e5096dda485246f
Ola <olawunmi263@yahoo.com>
2020-08-30 04:16:23 +0100

    Gross Pay Calculator Example

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,24 @@
+package chapter2;
+// Let's say we need to calculate an employee's gross pay.
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+
+//get the number of hours worked.
+        System.out.println("How many hours did you work?");
+        Scanner scanner = new Scanner(System.in);
+        int hoursWorked = scanner.nextInt();
+
+//Then, we get the hourly pay rate.
+        System.out.println("How much do you earn per hour?");
+      int hourlyPay =  scanner.nextInt();
+      scanner.close();
+
+//Next, we multiply the hours and the pay rate.
+        int grossPay = hourlyPay * hoursWorked;
+//Finally, we would display the result.
+        System.out.println("Your gross pay is £ " + grossPay);
+    }
+}
