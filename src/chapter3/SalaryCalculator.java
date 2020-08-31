4045b15e1afc0b224e38595b7d6db01fcdd44433
Ola <olawunmi263@yahoo.com>
2020-08-31 05:13:48 +0100

    Salary Calculator Example

---
+++ src/chapter3/SalaryCalculator.java
@@ -0,0 +1,22 @@
+package chapter3;
+/*
+Let's say we have a team of salespeople, all of whom make a standard rate of $1,000 a week.
+For any of them who make more than 10 sales that week, they get an additional bonus of $250.
+ */
+
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        int rate = 1000;
+        int targetSales = 10;
+        int bonus = 250;
+        System.out.println("Please enter number of sales");
+        Scanner scanner = new Scanner(System.in);
+         int numberOfSales = scanner.nextInt();
+         if (numberOfSales>targetSales){
+             rate = rate + bonus;
+         }
+        System.out.println("Your salary is £ "+ rate);
+    }
+}
