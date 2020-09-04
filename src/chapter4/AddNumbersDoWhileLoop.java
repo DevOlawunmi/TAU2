0fd192c22ec62b1217d1ed451cea0cad783f7cb6
Ola <olawunmi263@yahoo.com>
2020-09-04 05:11:56 +0100

    Do While Statement Practise Update

--- src/chapter4/AddNumbersDoWhileLoop.java
+++ src/chapter4/AddNumbersDoWhileLoop.java
@@ -3,6 +3,10 @@ package chapter4;
 Let's write a program that allows a user to calculate the sum of two numbers as many times as they like to.
 
 For this, we'll use the Do While Loop.
+
+******Just like the While Loop, it's also controlled by a condition.
+That condition is tested after the completion of the loop. So, the loop will always run at least once.
+So, it's good to use the do while loop when the loop should run at least one time, and based on the situation, they now need to repeat.
  */
 
 import java.util.Scanner;
