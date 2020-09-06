3e6b54ecb991620efdb3dcf8d12067d0ce46fa58
Ola <olawunmi263@yahoo.com>
2020-09-06 03:55:19 +0100

    Greeting method

---
+++ src/chapter5/Greeting.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        greetUser(name);
+        scanner.close();
+
+
+    }
+
+    public static void greetUser(String name){
+
+        System.out.println("Fine morning to you "+ name);
+    }
+}
