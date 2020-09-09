4537c8a16a785b0cb8be4118b8421715dd813325
Ola <olawunmi263@yahoo.com>
2020-09-09 05:00:51 +0100

    Calculate Area Example 2

---
+++ src/chapter6/HomeAreaCalculatorRedo.java
@@ -0,0 +1,27 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class HomeAreaCalculatorRedo {
+
+   static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Rectangle kitchen = getRoom();
+        Rectangle patio = getRoom();
+        double area = calculateTotalArea(kitchen,patio);
+        System.out.println(area);
+    }
+
+    private static Rectangle getRoom() {
+        System.out.println("Enter the length of the room");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of the room");
+        double width = scanner.nextDouble();
+        return new Rectangle(length,width);
+
+    }
+
+    private static double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2){
+        return rectangle1.calculateArea()+rectangle2.calculateArea();
+    }
+}
