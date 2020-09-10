375ea12f373c46187f6fcc7287a41a7e88accaf3
Ola <olawunmi263@yahoo.com>
2020-09-10 06:24:10 +0100

    Phone bill Runner

---
+++ src/chapter6/PhoneBillRunner.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class PhoneBillRunner {
+    public static void main(String[] args) {
+        PhoneBill bill1 = new PhoneBill(001,25,200,207);
+        bill1.printItemizedBill();
+    }
+}
