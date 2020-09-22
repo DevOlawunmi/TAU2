6db42bdf9123940cfd2f487640f46cb5da269f62
Ola <olawunmi263@yahoo.com>
2020-09-22 04:17:29 +0100

    Bank Account App  object created and successfully ran

--- src/miniprojects/BankAccount.java
+++ src/miniprojects/BankAccount.java
@@ -1,5 +1,7 @@
 package miniprojects;
 
+import java.util.Scanner;
+
 public class BankAccount {
     int balance;
     int previousTransaction;
@@ -33,5 +35,72 @@ public class BankAccount {
             System.out.println("Withdrawn: "+ Math.abs(previousTransaction));
         else System.out.println("No transaction took place");
     }
+
+    void showMenu(){
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome "+ customerName);
+        System.out.println("Your ID is "+ customerID);
+        System.out.println();
+        System.out.println("A: Check Balance");
+        System.out.println("B: Deposit");
+        System.out.println("C: Withdraw");
+        System.out.println("D: Previous Transaction");
+        System.out.println("E: Exit");
+
+        do {
+            System.out.println("=======================================================");
+            System.out.println("Select an option");
+            System.out.println("=======================================================");
+            option = scanner.next().charAt(0);
+            System.out.println();
+            switch (option){
+                case 'A':
+                    System.out.println("-------------------------------------");
+                    System.out.println("Balance = "+ balance);
+                    System.out.println("--------------------------------------");
+                    System.out.println();
+                    break;
+
+                case 'B':
+                    System.out.println("-------------------------------------");
+                    System.out.println("Enter an amount to deposit");
+                    System.out.println("--------------------------------------");
+                    int amount = scanner.nextInt();
+                    deposit(amount);
+                    System.out.println();
+                    break;
+                case 'C':
+                    System.out.println("-------------------------------------");
+                    System.out.println("Enter an amount to withdraw");
+                    System.out.println("--------------------------------------");
+                    int amount2 = scanner.nextInt();
+                    withdraw(amount2);
+                    System.out.println();
+                    break;
+                case 'D':
+                    System.out.println("-------------------------------------");
+                    getPreviousTransaction();
+                    System.out.println("--------------------------------------");
+                    System.out.println();
+                    break;
+
+                case 'E':
+                    System.out.println("***********************************");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please select another option");
+                    break;
+
+            }
+
+
+        }while (option!='E');
+        System.out.println("Thank you for using our services");
+
+
+
+    }
+
 
 }
--- src/miniprojects/BankingApplication.java
+++ src/miniprojects/BankingApplication.java
@@ -3,5 +3,7 @@ package miniprojects;
 public class BankingApplication {
     public static void main(String[] args) {
         BankAccount ola = new BankAccount("Ola", "263");
+        ola.showMenu();
+
     }
 }
