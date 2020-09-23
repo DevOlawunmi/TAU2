6cfd4ea62a1615d64ab8532a64ef436bb48a603d
Ola <olawunmi263@yahoo.com>
2020-09-23 06:04:13 +0100

    Email app 1

---
+++ src/miniprojectemailadminapp/Email.java
@@ -0,0 +1,58 @@
+package miniprojectemailadminapp;
+/*
+Scenario: An IT support admin charged with the task of creating email addresses for new hires.
+Your app should do the ff
+Generate an email with the ff syntax firstname.lastname@department.company.com
+Determine the department (sales, dev, accounting), if none leave blank
+Generate a random string for a password
+Have set methods to change the password, set the mailbox capacity and define an alternate email address
+Have get methods to display the name, email and mailbox capacity
+ */
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String  password;
+    private int mailboxCapacity;
+    private String alternateEmail;
+    // constructor to receive the first name and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: "+ this.firstName+ "."+ this.lastName);
+        // call a method asking for the department - return the department
+        this.department= setDepartment();
+        System.out.println("Department: "+ department);
+        //call a method that returns a random password
+    }
+
+
+
+    //ask for the department
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES : \n1 for Sales \n2 for Dev \n3 for Accounting \n0 for none \n Enter department code");
+        Scanner scanner = new Scanner(System.in);
+        int departmentChoice = scanner.nextInt();
+        if (departmentChoice == 1){ return "sales";}
+        else if (departmentChoice==2){ return "dev"; }
+        else if (departmentChoice==3){ return "accounting"; }
+        else return "";
+    }
+
+    //generate the password
+
+    //write method to change password
+
+    //set mailbox capacity
+
+    //set alternate email
+
+    //display email
+
+    //display name
+
+    //display mailbox capacity
+}
---
+++ src/miniprojectemailadminapp/EmailApp.java
@@ -0,0 +1,19 @@
+package miniprojectemailadminapp;
+/*
+Scenario: An IT support admin charged with the task of creating email addresses for new hires.
+Your app should do the ff
+Generate an email with the ff syntax firstname.lastname@department.company.com
+Determine the department (sales, dev, accounting), if none leave blank
+Generate a random string for a password
+Have set methods to change the password, set the mailbox capacity and define an alternate email address
+Have get methods to display the name, email and mailbox capacity
+ */
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email email1 = new Email("Ola","Ajibola");
+
+
+    }
+}
+
